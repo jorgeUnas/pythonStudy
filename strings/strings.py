d0d40749c06c6df6471b00c28487d56092de6d77
EC2 Default User <ec2-user@ip-172-31-35-57.eu-west-2.compute.internal>
2024-10-08 14:40:13 +0000

    joining a list to get lines of string

--- strings/strings.py
+++ strings/strings.py
@@ -49,3 +49,10 @@ reapers_line_one_words = ["Black", "reapers", "with", "the", "sound", "of", "ste
 
 reapers_line_one = ' '.join(reapers_line_one_words)
 print(reapers_line_one)
+
+
+winter_trees_lines = ['All the complicated details', 'of the attiring and', 'the disattiring are completed!', 'A liquid moon', 'moves gently among', 'the long branches.', 'Thus having prepared their buds', 'against a sure winter', 'the wise trees', 'stand sleeping in the cold.']
+
+winter_trees_full = '\n'.join(winter_trees_lines)
+
+print(winter_trees_full)
