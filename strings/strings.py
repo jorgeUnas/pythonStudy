2d6f83cec359390d95ce5bb35e86f978c568ed4f
EC2 Default User <ec2-user@ip-172-31-35-57.eu-west-2.compute.internal>
2024-10-08 14:23:36 +0000

    splitting a string line by line

--- strings/strings.py
+++ strings/strings.py
@@ -15,3 +15,29 @@ def password_generator(user_name):
     for i in range(0, len(user_name)):
         password += user_name[i-1]
     return password
+    
+    
+#Splitting strings 
+
+spring_storm_text = \
+"""The sky has given over 
+its bitterness. 
+Out of the dark change 
+all day long 
+rain falls and falls 
+as if it would never end. 
+Still the snow keeps 
+its hold on the ground. 
+But water, water 
+from a thousand runnels! 
+It collects swiftly, 
+dappled with black 
+cuts a way for itself 
+through green ice in the gutters. 
+Drop after drop it falls 
+from the withered grass-stems 
+of the overhanging embankment."""
+
+spring_storm_lines = spring_storm_text.split('\n')
+
+print(spring_storm_lines)
