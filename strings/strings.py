631b15973e32f704947ea0af881402b12e3a4cf2
EC2 Default User <ec2-user@ip-172-31-35-57.eu-west-2.compute.internal>
2024-10-08 14:45:07 +0000

    Using the format method to create a string

--- strings/strings.py
+++ strings/strings.py
@@ -56,3 +56,12 @@ winter_trees_lines = ['All the complicated details', 'of the attiring and', 'the
 winter_trees_full = '\n'.join(winter_trees_lines)
 
 print(winter_trees_full)
+
+
+#Format method
+
+def poem_title_card(title, poet):
+  poem_desc = "The poem \"{}\" is written by {}.".format(title, poet)
+  return poem_desc
+
+print(poem_title_card("I Hear America Singing","Walt Whitman"))
