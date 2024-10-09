588bc188d2db4b8e2346b344097679dbcf7e666e
EC2 Default User <ec2-user@ip-172-31-35-57.eu-west-2.compute.internal>
2024-10-09 13:46:14 +0000

    Another way to use format

--- strings/strings.py
+++ strings/strings.py
@@ -65,3 +65,18 @@ def poem_title_card(title, poet):
   return poem_desc
 
 print(poem_title_card("I Hear America Singing","Walt Whitman"))
+
+# Another way to use format
+
+def poem_description(publishing_date, author, title, original_work):
+  poem_desc = "The poem {title} by {author} was originally published in {original_work} in {publishing_date}.".format(publishing_date = publishing_date, author = author, title = title, original_work = original_work)
+  return poem_desc
+
+author = "Shel Silverstein"
+title = "My Beard"
+original_work = "Where the Sidewalk Ends"
+publishing_date = "1974"
+
+my_beard_description = poem_description(publishing_date, author, title, original_work)
+
+print(my_beard_description)
