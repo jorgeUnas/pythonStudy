82f4a4edd43fe49485f7ddb464039ef4823295a7
EC2 Default User <ec2-user@ip-172-31-35-57.eu-west-2.compute.internal>
2024-10-11 11:57:58 +0000

    Adding keys to empty dictionaries

--- dictionaries/dictionaries.py
+++ dictionaries/dictionaries.py
@@ -2,3 +2,12 @@ sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22}
 num_cameras = {"backyard": 6,  "garage": 2, "driveway": 1}
 
 print(sensors)
+
+
+#Adding keys to empty dictionaries 
+
+animals_in_zoo = {}
+animals_in_zoo['zebras'] = 8
+animals_in_zoo['monkeys'] = 12
+animals_in_zoo['dinosaurs'] = 0
+print(animals_in_zoo)
